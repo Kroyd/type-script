1e3273eb43b2541ce02e5228df23138e70a9c483
kroyd <kroydjaykven@gmail.com>
2023-08-10 18:44:38 +0500

    ts

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
+  <h1 class="mt-[50px] text-[#fff]">Tailwind</h1>
   <router-view/>
 </template>
 
