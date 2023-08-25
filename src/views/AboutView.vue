16daba0cc0880d71931211bb4088d8f2f5d4be19
kroyd <kroydjaykven@gmail.com>
2023-08-25 17:05:06 +0500

    TS-2

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,39 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="card">
+    <h1>Пользователи</h1>
+
+      <p style="color: red;" v-if="error">{{ error }}</p>
+
+
+    <suspense v-else>
+      <template #default>
+        <hello-world></hello-world>
+      </template>
+      <template #fallback>
+        <p>Loading...</p>
+      </template>
+    </suspense>
   </div>
 </template>
+
+
+<script lang="ts">
+import { defineComponent, onErrorCaptured, ref, } from 'vue';
+import HelloWorld from '@/components/HelloWorld.vue';
+export default defineComponent({
+  components: {
+    HelloWorld
+  },
+  setup() {
+    const error =  ref<null | unknown>(null)
+
+    onErrorCaptured( e => {
+      error.value = e
+      return true
+    })
+    return {
+      error
+    }
+  },
+})
+</script>
